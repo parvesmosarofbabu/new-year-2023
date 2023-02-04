e517a3e65d8580e81eb8d7f616e9540dd36bb7e6
parvesmosarofbabu <77316098+parvesmosarofbabu@users.noreply.github.com>
2023-02-04 22:30:56 +0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function() {	
+
+		// Scroll to Top
+		jQuery('.scrolltotop').click(function(){
+			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+			return false;
+		});
+		
+		jQuery(window).scroll(function(){
+			var upto = jQuery(window).scrollTop();
+			if(upto > 500) {
+				jQuery('.scrolltotop').fadeIn();
+			} else {
+				jQuery('.scrolltotop').fadeOut();
+			}
+		});
+
+		
+	$('#dolor').modally();
+    modally('video-modal', {video: true, 'max-width': 800});
+
+
+ 
+
+		
+});
